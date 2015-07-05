e63b5d9ec4651d6fd7099023d7ea69df8fed75c4
xwertx <rmadejowicz@tlen.pl>
2015-07-05 08:06:17 -0700

    create views and edit match_controller for new match forms

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -1,30 +1,38 @@
 class MatchesController < ApplicationController
+
   respond_to :html
 
+  def index
+    @matches = Match.all
+  end
+
+  def show
+    @match = Match.find(params[:id])
+  end
+
+  def new
+    @match = Match.new
+    @players = Player.all
+    2.times {@match.match_edits.build}
+  end
+
   def create
-    match.update match_params
-    respond_with match, location: [:matches]
+    @match = Match.new(match_params)
+    respond_to do |format|
+      if @match.save
+        format.html { redirect_to @match, notice: 'Match was successfully created.' }
+        format.json { render :show, status: :created, location: @match }
+      else
+        format.html { render :new }
+        format.json { render json: @match.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   private
 
-  def match
-    @match = Match.new
-  end
-  helper_method :match
-
   def match_params
-    params[:match].permit :match_date, player_ids: []
+    params[:match].permit :match_date, player_ids: [], results: [], :match_edits_attributes => [:id, :result]
   end
-
-  def matches
-    @matches = Match.all
-  end
-  helper_method :matches
-
-  def players
-    @players = Player.all
-  end
-  helper_method :players
 
 end
--- app/models/match.rb
+++ app/models/match.rb
@@ -2,5 +2,6 @@ class Match < ActiveRecord::Base
 
 	has_many :match_edits
 	has_many :players, through: :match_edits
+	accepts_nested_attributes_for :match_edits
 
 end

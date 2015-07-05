311516ca8a4bce9e9b333213031ad106b411902d
xwertx <rmadejowicz@tlen.pl>
2015-07-05 08:50:16 -0700

    add match history forplayers

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -8,6 +8,7 @@ class MatchesController < ApplicationController
 
   def show
     @match = Match.find(params[:id])
+    @results = @match.match_edits
   end
 
   def new
--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -10,6 +10,7 @@ class PlayersController < ApplicationController
   # GET /players/1
   # GET /players/1.json
   def show
+    @matches = @player.matches
   end
 
   # GET /players/new
